0358dbaaeaa9c35bc7ceb3995e8b451db19d00a9
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-09-02 22:10:51 -0400

    Realizando la interfaz del login de usuario

---
+++ src/components/Login.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Iniciar Sesión</div>
+          <div class="card-body">
+            <form class="col s12" @submit.prevent="login">
+              <!--h2 class="form-signin-heading">Please sign in</h2-->
+              <div class="alert alert-danger" v-if="error">{{ error }}</div>
+              <div class="form-group row">
+                <label for="inputEmail" class="col-sm-4 col-form-label text-md-right">Correo Electrónico</label>
+                <div class="col-md-6">
+                  <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Correo Electrónico" required autofocus>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputPassword" class="col-md-4 col-form-label text-md-right">Contraseña</label>
+                <div class="col-md-6">
+                  <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                </div>
+              </div>
+              <div class="form-group row mb-0">
+                <div class="col-md-8 offset-md-4">
+                  <button class="btn btn-primary row" type="submit">Iniciar Sesión</button>
+                </div>
+              </div>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: false
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.email)
+      console.log(this.password)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+body {
+  background: #605B56;
+}
+
+.login-wrapper {
+  background: #fff;
+  width: 70%;
+  margin: 12% auto;
+}
+
+.form-signin {
+  max-width: 330px;
+  padding: 10% 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
