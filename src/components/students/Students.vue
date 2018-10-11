91eba2f3c8acb9c6b4d7dcf3f226f4985cf2a3e5
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-10-10 20:16:21 -0400

    Carga de archivo de prueba

--- src/components/students/Students.vue
+++ src/components/students/Students.vue
@@ -1,11 +1,68 @@
 <template>
-    <div>
-        <h1>Vista para los estudiantes</h1>
-    </div>
+	<div class="container">
+		<div class="large-12 medium-12 small-12 cell">
+		  <label>Archivo
+		    <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+		  </label>
+		  <button v-on:click="submitFile()">Enviar</button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-}
+import axios from 'axios';
+
+  export default {
+    /*
+      Defines the data used by the component
+    */
+    data(){
+      return {
+        file: ''
+      }
+    },
+
+    methods: {
+      /*
+        Submits the file to the server
+      */
+      submitFile(){
+        /*
+                Initialize the form data
+            */
+            let formData = new FormData();
+
+            /*
+                Add the form data we need to submit
+            */
+            formData.append('file', this.file);
+
+        /*
+          Make the request to the POST /single-file URL
+        */
+            const path = 'http://localhost:8082/upload';
+            axios.post(path,
+                formData,
+                {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+              }
+            ).then(function(){
+          console.log('SUCCESS!!');
+        })
+        .catch(function(){
+          console.log('FAILURE!!');
+        });
+      },
+
+      /*
+        Handles a change on the file upload
+      */
+      handleFileUpload(){
+        this.file = this.$refs.file.files[0];
+      }
+    }
+  }
 </script>
 
