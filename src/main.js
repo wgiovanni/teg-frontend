ca1083d95cfbe85aebfd47ce966a0e98d7a82ce3
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-11-18 19:20:37 -0400

    quitando Bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
 import App from './App'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
 import { router } from './router'
 import store from './store'
 
